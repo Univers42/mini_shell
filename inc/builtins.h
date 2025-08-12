dbb3f6ce312f4efe430e5f09f1fee1b029ae752f
LESdylan <dev.pro.photo@gmail.com>
2025-08-12 13:08:37 +0200

    correct simple structure masking to hnalde combinations and call of functions well possibly calls of functions

--- inc/builtins.h
+++ inc/builtins.h
@@ -14,36 +14,41 @@ typedef enum
 	FLAG_COUNT = 6
 }t_builtin_flags;
 
-typedef struct 
+typedef char	t_env;
+typedef int		(*t_builtin)(char **args, int flags, t_env *env);
+
+// Builtin struct
+typedef struct s_builtins
 {
-	t_string	name;
-	int		(*builtin)(char **args, int flags, t_env **env);
-	int		valid_flags;
-}			t_builtin;
+    t_string	name;
+    t_builtin	builtin;
+    int			valid_flags;
+}				t_builtins;
 
-typedef int (*builtin)(char **args, int flags, t_env *env);
+// Builtin function declarations
+int bin_echo(char **args, int flags, t_env *env);
+int bin_cd(char **args, int flags, t_env *env);
+int bin_pwd(char **args, int flags, t_env *env);
+int bin_export(char **args, int flags, t_env *env);
+int bin_unset(char **args, int flags, t_env *env);
+int bin_env(char **args, int flags, t_env *env);
+int bin_exit(char **args, int flags, t_env *env);
 
-builtin	echo(char **args, int flags, t_env *env);
-buitin	cd(char **args, int flags, t_env *env);
-builtin	pwd(char **args, int flags, t_env *env);
-builtin	export(char **args, int flags, t_env *env);
-builtin	unset(char **args, int flags, t_env *env);
-builtin env(char **args, int flags, t_env *env);
-builtin	exit(char **args, int flags, t_env *env);
+// Builtins array and getter
+t_builtins *access_builtins(void)
+{
+	static t_builtins bins[] = {
+    {"echo", bin_echo, ECHO_N},
+    {"cd", bin_cd, CD_L | CD_P},
+    {"pwd", bin_pwd, 0},
+    {"export", bin_export, EXPORT_P},
+    {"unset", bin_unset, UNSET_V | UNSET_F},
+    {"env", bin_env, 0},
+    {"exit", bin_exit, 0},
+    {NULL, NULL, 0}
+	};
 
-t_builtin       gbuiltin()                 {
-        static t_builtin a[] =
-        {
-                {"echo", echo, ECHO_N};
-                {"cd", cd};
-                {"pwd", pwd, 0};
-                {"export", export, 0};
-                {"unset", unset, };
-                {"exit", exit, 0};
-                {NULL, NULL, 0};
-        };
-
-        return (&a);
+	return (&bins);
 }
 
 typedef enum e_cmd_idx
@@ -57,4 +62,5 @@ typedef enum e_cmd_idx
 	BIN_EXIT,
 	BIN_COUNT
 }	t_cmd_idx;
+
 #endif
