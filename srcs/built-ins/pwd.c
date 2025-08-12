cde94e2e5ce95cc8ccc9a4069711604042c91aaa
danielm3 <danielm3@student.42madrid.com>
2025-08-12 20:38:52 +0200

    pwd implemented and tested with Copilot

--- srcs/built-ins/pwd.c
+++ srcs/built-ins/pwd.c
@@ -1,10 +1,40 @@
 #include "builtins.h"
 
-int bin_pwd(char **args, int flags, t_env *env)
+static char *extract_env_value(const char *name, char **envp)
 {
+	size_t len = ft_strlen(name);
+	size_t i = 0;
+	char   *eq;
+
+	while (envp[i])
+	{
+		eq = ft_strchr(envp[i], '=');
+		if (eq && (size_t)(eq - envp[i]) == len
+			&& ft_strncmp(envp[i], name, len) == 0)
+			return (eq + 1);
+		i++;
+	}
+	return (NULL);
+}
+
+
+int	bin_pwd(char **args, int flags, t_env *env)
+{
+	char	*pwd;
+	char	*cwd;
+
 	(void)args;
 	(void)flags;
-	(void)env;
-	printf("pwd builtin called\n");
-	return 0;
+	pwd = extract_env_value("PWD", env->envp);
+	if (pwd)
+	{
+		ft_putendl_fd(pwd, 1);
+		return (0);
+	}
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+		return (1);
+	ft_putendl_fd(cwd, 1);
+	free(cwd);
+	return (0);
 }
