24386d245fe9dec7ae5e08ebf9d940fb812b3d47
LESdylan <dev.pro.photo@gmail.com>
2025-08-13 23:57:53 +0200

    add comment

---
+++ srcs/execution/execution.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syzygy <syzygy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 23:57:03 by syzygy            #+#    #+#             */
+/*   Updated: 2025/08/13 23:57:39 by syzygy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+Runs the program (like `echo` with the final argument list -- no quotes
+no unprocessed $var, just plain strings)
+*/
