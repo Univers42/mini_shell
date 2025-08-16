f4a0a23759f6a8d377f61504b827fb63ab8891db
LESdylan <dev.pro.photo@gmail.com>
2025-08-16 12:43:56 +0200

    update

---
+++ srcs/feats/render.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syzygy <syzygy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/14 15:41:23 by syzygy            #+#    #+#             */
+/*   Updated: 2025/08/14 18:02:03 by syzygy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "render.h"
+
+/* Provide globals used by the renderer and prompt logic. */
+render_mode_t g_render_mode = RENDER_FANCY;
+int           g_last_status = 0;
