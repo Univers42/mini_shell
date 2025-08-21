c1c172e7403b37ce5a80c492f3ba9e5d9516d428
Dylan Lesieur <dlesieur@c1r3s6.42madrid.com>
2025-08-21 02:38:34 +0200

    update

--- srcs/execution/exec_internal.c
+++ srcs/execution/exec_internal.c
@@ -6,7 +6,7 @@
 /*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 22:07:57 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/08/20 22:47:33 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/08/21 02:38:14 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,42 +21,17 @@
 #include "builtins.h"
 #include "minishell.h"
 
-// ------------------- main entry ---------------------
-/** 
- * argv is already the correct vector for execve (argv[0] = cmd)
- * 
-*/
-int	exec_internal(int argc, char **argv, char **envp)
+static int	handle_execve(char *path, char **argv, char **envp)
 {
-	pid_t	pid;
-	int		status;
-	char	*path;
+	execve(path, argv, envp);
+	perror("execve");
+	_exit(126);
+}
 
-	status = 0;
-	if (argc <= 0 || !argv || !argv[0])
-		return (0);
-	path = resolve_exec_path(argv[0], envp);
-	if (!path)
-	{
-		ft_fprintf(2, "Unknown command: %s\n", argv[0]);
-		ms()->last_status = 127;
-		return (0);
-	}
-	pid = fork();
-	if (pid < 0)
-	{
-		perror("fork");
-		free(path);
-		ms()->last_status = 1;
-		return (0);
-	}
-	if (pid == 0)
-	{
-		execve(path, argv, envp);
-		perror("execve");
-		_exit(126);
-	}
-	free(path);
+static int	handle_wait_status(int pid)
+{
+	int	status;
+
 	if (waitpid(pid, &status, 0) < 0)
 	{
 		perror("waitpid");
@@ -70,4 +45,38 @@ int	exec_internal(int argc, char **argv, char **envp)
 	else
 		ms()->last_status = 1;
 	return (1);
+}
+
+static int	launch_exec(char *path, char **argv, char **envp)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		free(path);
+		ms()->last_status = 1;
+		return (0);
+	}
+	if (pid == 0)
+		handle_execve(path, argv, envp);
+	free(path);
+	return (handle_wait_status(pid));
+}
+
+int	exec_internal(int argc, char **argv, char **envp)
+{
+	char	*path;
+
+	if (argc <= 0 || !argv || !argv[0])
+		return (0);
+	path = resolve_exec_path(argv[0], envp);
+	if (!path)
+	{
+		ft_fprintf(2, "Unknown command: %s\n", argv[0]);
+		ms()->last_status = 127;
+		return (0);
+	}
+	return (launch_exec(path, argv, envp));
 }
